6e636dfce08b900f510f57ed5547015f829f5ac7
emelenium <emelenium@gmail.com>
2021-01-10 16:27:53 +0500

    исправленная отправка сообщеения

--- src/main/java/ru/home/mytelegram_bot/MyWizardTelegramBot.java
+++ src/main/java/ru/home/mytelegram_bot/MyWizardTelegramBot.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -33,6 +34,18 @@ public class MyWizardTelegramBot extends TelegramWebhookBot {
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
         if (update.getMessage() !=null && update.getMessage().hasText()){
         long chat_id = update.getMessage().getChatId();
+
+        Message message = update.getMessage();//создается объект телеграмовское сообщение из переданного update
+        SendMessage sendMessage = new SendMessage();//инициализация, отправляемого сообщения, создаем объект SendMessage библиотеки telegram
+        sendMessage.enableMarkdown(true);//включаем возможность разметки
+        sendMessage.setChatId(message.getChatId().toString());//устанавливаем id чата в который будем
+        sendMessage.setReplyToMessageId(message.getMessageId());//установка id сообщения, на которое будем отвечать
+        sendMessage.setText("Hi"+update.getMessage().getText());
+            try {
+                execute(sendMessage);//непосредственная реализация отправки сообщения
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
 /*
          try {
              execute(new SendMessage(chat_id,"Hi")+update.getMessage().getText());
